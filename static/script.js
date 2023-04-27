6d14ca8d13505f7e9b3c17c6976e4ef3c4805b3e
Mamata <100906354+mamata171@users.noreply.github.com>
2023-04-27 11:18:00 +0530

    first commit

---
+++ static/script.js
@@ -0,0 +1,20 @@
+console.log("hello world")
+
+function myfun(){
+    element = document.getElementById("hidden")
+    if (element.style.display === "none") {
+        element.style.display = "block";
+      } else {
+        element.style.display = "none";
+      }
+    
+}
+
+function musicfun(){
+  element = document.getElementById("photos")
+    if (element.style.display === "none") {
+        element.style.display = "block";
+      } else {
+        element.style.display = "none";
+      }
+}
