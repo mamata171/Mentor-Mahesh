5027c4a9864365163c699020e951ee7f9d5210ec
Mamata <100906354+mamata171@users.noreply.github.com>
2023-04-27 16:45:29 +0530

    commit

--- static/script.js
+++ static/script.js
@@ -1,20 +1,45 @@
 console.log("hello world")
 
-function myfun(){
-    element = document.getElementById("hidden")
-    if (element.style.display === "none") {
-        element.style.display = "block";
-      } else {
-        element.style.display = "none";
-      }
+// function show(){
+//   console.log("show fun triggered")
+//     element = document.getElementById("hidden")
+//     console.log(element.style.display)
+//     if (element.style.display === "none") {
+//         element.style.display = "block";
+//       } 
+//       console.log(element.style.display)
     
+// }
+
+function show() {
+  console.log("show fun triggered");
+  var element = document.getElementById("hidden");
+  console.log(element.style.display); // Check initial value
+  if (element.style.display === "" || element.style.display === "none") {
+    element.style.display = "block";
+  }
+  console.log(element.style.display); // Check updated value
+}
+
+function hide(){
+  console.log("hide fun triggered")
+  element = document.getElementById("hidden")
+  if (element.style.display === "block") {
+    element.style.display = "none";
+  } 
 }
 
 function musicfun(){
   element = document.getElementById("photos")
-    if (element.style.display === "none") {
-        element.style.display = "block";
-      } else {
-        element.style.display = "none";
-      }
+  if (element.style.display === "" || element.style.display === "none") {
+    element.style.display = "block";
+  }
+}
+
+function musichide() {
+  element = document.getElementById("photos")
+  if (element.style.display === "block") {
+    element.style.display = "none";
+  } 
+
 }
